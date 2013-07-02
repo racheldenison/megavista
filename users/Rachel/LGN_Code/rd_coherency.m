66425307ce96a764eca7448a27281bd87004fad9
Rachel Denison <r.n.denison@gmail.com>
2013-07-01 20:17:06 -0700

    added coherency to rd_mrMeanTSeriesCorrelation.m. this invoved making a standalone function, rd_coherency.m, that calculates coherency between all the columns of a matrix

---
+++ users/Rachel/LGN_Code/rd_coherency.m
@@ -0,0 +1,33 @@
+function [meanCoherence, meanPhase] = rd_coherency(A, freqRange, window, noverlap, Fs)
+
+if notDefined('window'),       window = []; end
+if notDefined('noverlap'),   noverlap = []; end %TODO: what should this be?
+if notDefined('Fs'),               Fs = []; end
+
+% one column of A at a time
+for i = 1:size(A, 2)  
+    % [pxx,f] = pwelch(x,window,noverlap,f,fs)
+    [Pxx(:,i), F] = pwelch(A(:,i), window, noverlap, [], Fs);
+end
+
+emptyMatrix = zeros(size(F, 1), size(A, 2), size(A, 2));
+Pxy         = emptyMatrix;
+coherence   = emptyMatrix;
+phase       = emptyMatrix;
+
+for i = 1:size(A, 2)
+    for j = 1:size(A, 2) % i:size(A, 2) to do half the work
+        Pxy(:, j, i) = cpsd(A(:,i), A(:,j), window, noverlap, [], Fs);
+        coherence(:, j, i) = (abs(Pxy(:, j, i)) .^ 2) ./ (Pxx(:,i) .* Pxx(:,j));
+        phaseRad = angle(Pxy(:, j, i)); % in radians
+        phase(:, j, i) = phaseRad./(2*pi*F);
+    end
+end
+
+% Take only those frequencies in the specified range
+if notDefined('freqRange')
+    freqRange = [F(1) F(end)]; 
+end
+
+meanCoherence = squeeze(mean(coherence(F>=freqRange(1) & F<=freqRange(2),:,:)));
+meanPhase     = squeeze(mean(phase(F>=freqRange(1) & F<=freqRange(2),:,:)));
--- users/Rachel/LGN_Code/rd_mrMeanTSeriesCorrelation.m
+++ users/Rachel/LGN_Code/rd_mrMeanTSeriesCorrelation.m
@@ -1,15 +1,59 @@
 % rd_mrMeanTSeriesCorrelation.m
 
+%% setup
 dt = 1;
 scan = 1;
-rois = {'ROI1','ROI2','ROI3'};
+% rois = {'ROI101','LV1','L_hMTplus','ROI201','RV1','R_hMTplus'};
+rois = {'LLGN_ecc0','LLGN_ecc14','LV1_ecc0-2','LV1_ecc10-18',...
+    'RLGN_ecc2','RLGN_ecc9','RV1_ecc1-3','RV1_ecc7-11'};
 
 getRawData = 1;
 
-% Open hidden Inplane
+%% Open hidden Inplane
 vw = initHiddenInplane(dt, scan, rois);
 
+%% Get ROI mean tseries
 [roiTSeries, tSerr] = meanTSeries(vw, scan, rois, getRawData);
 
+%% Calulcate correlation between all tseries
 roiTSeries = cell2mat(roiTSeries);
 roiCorr = corr(roiTSeries);
+
+%% Plot
+f = figure;
+clim = rd_zeroCenterCLim(roiCorr);
+% imagesc(tril(roiCorr),clim);
+imagesc(roiCorr,clim);
+axis equal
+axis tight
+title('Correlation',...
+    'Color','k','FontSize',12,'FontWeight','demi');
+colormap(rdbumap)
+colorbar
+set(gca,'XTickLabel',rois)
+set(gca,'YTickLabel',rois)
+
+
+%% Calculate coherency between all tseries
+freqRange = [0.01 0.15];
+Fs = 1/mrSESSION.functionals(scan).framePeriod; % 1/TR
+
+% calculates coherence and phase between all columns of roiTSeries
+[roiCoh, roiPhase] = rd_coherency(roiTSeries, freqRange, [], [], Fs);
+
+%% Plot
+f = figure;
+clim = rd_zeroCenterCLim(roiCoh);
+imagesc(roiCoh,clim);
+axis equal
+axis tight
+title('Coherence',...
+    'Color','k','FontSize',12,'FontWeight','demi');
+colormap(rdbumap)
+colorbar
+set(gca,'XTickLabel',rois)
+set(gca,'YTickLabel',rois)
+
+
+
+
